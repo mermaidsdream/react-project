5f475962472c7c767f7adc02bbe40de5a7ad7e25
Vasylyna Lykhogodenko <vvasylyna@yahoo.com>
2022-04-01 21:00:15 +0300

    feat: add solution in javascript

--- task1/index.js
+++ task1/index.js
@@ -1,1 +1,22 @@
+function calculate(str) {
+  let result;
+  let [leftNumber, operator, rightNumber] = str.split(' ');
 
+  if (operator === '+') {
+    return result = '.'.repeat(leftNumber.length + rightNumber.length);
+  }
+
+  if (operator === '-') {
+    return result = '.'.repeat(leftNumber.length - rightNumber.length);
+  }
+
+  if (operator === '*') {
+    return result = '.'.repeat(leftNumber.length * rightNumber.length);
+  }
+
+  if (operator === '//') {
+    return result = '.'.repeat(leftNumber.length / rightNumber.length);
+  }
+}
+
+console.log(calculate("..... - ..."));
