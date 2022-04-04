d8b9094349733ff759a167ddcbaead4bad145dd8
Vasylyna Lykhogodenko <vvasylyna@yahoo.com>
2022-04-04 20:44:14 +0300

    feat: add comment

--- task2/index.js
+++ task2/index.js
@@ -1,20 +1,25 @@
-function alphabet(n){
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  let newArray = [];
-
-  if (n < 0) return null;
-
-  for (let i = 0; i < n; i++) {
-    if (i > -1) {
-      newArray.push('\n');
+function alphabet(n) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let newArray = [];
+  
+    if (n < 0) return null;
+  
+    for (let i = 0; i < n; i++) {
+      if (i > -1) {
+        newArray.push('\n');
+      }
+    
+      for (let j = 0; j < n; j++) {
+        newArray.push(alphabet[i + j]);
+      }
     }
   
-    for (let j = 0; j < n; j++) {
-      newArray.push(alphabet[i + j]);
-    }
+    return newArray.join(' ');
   }
+  
+  console.log(alphabet(4)); 
 
-  return newArray.join(' ');
-}
-
-console.log(alphabet(4));
+  // a b c d 
+  // b c d e 
+  // c d e f 
+  // d e f g
