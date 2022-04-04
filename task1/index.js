6128dd39d089c45a46de23e0252047abe66082d0
Vasylyna Lykhogodenko <vvasylyna@yahoo.com>
2022-04-04 20:40:20 +0300

    fix: fix mistakes in code

--- task1/index.js
+++ task1/index.js
@@ -1,22 +1,24 @@
-function calculate(str) {
-  let result;
-  let [leftNumber, operator, rightNumber] = str.split(' ');
-
-  if (operator === '+') {
-    return result = '.'.repeat(leftNumber.length + rightNumber.length);
-  }
-
-  if (operator === '-') {
-    return result = '.'.repeat(leftNumber.length - rightNumber.length);
-  }
-
-  if (operator === '*') {
-    return result = '.'.repeat(leftNumber.length * rightNumber.length);
-  }
-
-  if (operator === '//') {
-    return result = '.'.repeat(leftNumber.length / rightNumber.length);
-  }
-}
-
-console.log(calculate("..... - ..."));
+function calculate(sentence) {
+  
+  if (typeof sentence !== 'string') return '';
+  
+  const [leftNumber, operator, rightNumber] = sentence.split(' ');
+  
+    if (operator === '+') {
+      return '.'.repeat(leftNumber.length + rightNumber.length);
+    }
+  
+    if (operator === '-') {
+      return '.'.repeat(leftNumber.length - rightNumber.length);
+    }
+  
+    if (operator === '*') {
+      return '.'.repeat(leftNumber.length * rightNumber.length);
+    }
+  
+    if (operator === '//') {
+      return '.'.repeat(leftNumber.length / rightNumber.length);
+    }
+   }
+   
+   console.log(calculate("..... - ...")); // ..
